2f2710f5b069dcf349935127f0d79c3c2449e88c
GabbyHolics <karenthgabriela@gmail.com>
2021-08-24 18:13:09 -0400

    organize code and function typeFolderAndDocument

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-module.exports = () => {
-// recorrer todo y hacer una funcion que tenga todos los links. 
-// seguir trabanajndo con el parametro
- 
+// 1. obtener ruta
+const fs = require("fs");
+const path = require("path");
+const routeDirection = path.resolve(process.argv[2]);
+// 2. reconocer que es un archivo o documento
+const typeFolderAndDocument = () => {
+fs.stat(routeDirection, (error, stats) => {
+        if (error) {
+            console.warn('route invalid')
+            return(error);
+        }
+        if(stats.isFile()){
+            console.log("es un documento")
+            console.log(routeDirection)
+        }else if(stats.isDirectory()){
+            console.log("es un archivo")
+            console.log(routeDirection)
+        }
+    }
+)};
+typeFolderAndDocument()
+// if (documents) {
+//     fs.readFile(routeDirection, "utf8", (err, data) => {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//       console.log(data);
+//     });
+//     console.log(file);
+//   } else if (folder) {
+//     fs.readdir(routeDirection, (_err, files) => {
+//       files.forEach((file) => {
+//         console.log(file);
+//       });
+//     });
+//   }
 
-};
-console.log(process.argv[2]);
+
+// 3. necesito saber su extencion
+// 4.necesito que coloque un archivo valido
+// 5.leer documento.
+
+// Crea una funcion que tenga una condicion primero de verificar si es un archivo valido si es valido
+// si es una carpeta que recorra la carpeta y vea si tiene un archivo md/ si es un archivo verificar si es archivo md.
+//Si no cumple ninguno de los casos mandar un error.
+//Si cumple leer el documento y su contenido
+
+// module.exports = () => {
+//     // recorrer todo y hacer una funcion que tenga todos los links.
+//     }
--- training.js
+++ training.js
@@ -1,48 +1,70 @@
-//Como primer reto, puedes tratar de leer un solo archivo con una ruta fija e 
+//Como primer reto, puedes tratar de leer un solo archivo con una ruta fija e
 //imprimir su contenido en la consola con un console.log.
 
 // leer archivo
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
+console.log(process.argv);
+const documentDirection = path.resolve(process.argv[2]);
 
-const documentDirection = process.argv[2]
-
-const file = fs.readFile(documentDirection, 'utf8' , (err, data) => {
+const file = fs.readFile(documentDirection, "utf8", (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  console.log(data)
-})
-console.log(file)
+  console.log(data);
+});
+console.log(file);
 
 // tipo de dato
-
-const typePath = path.extname(documentDirection)
+const typePath = path.extname(documentDirection);
 console.log(typePath);
 
 // Intenta imprimir en consola la lista de archivos en una carpeta.
-const folder = process.argv[2];
-    fs.readdir(folder, (err,files) => {
-        files.forEach(file => {
-            console.log(file); 
-        });
-    });
+fs.readdir(folder, (err, files) => {
+  files.forEach((file) => {
+    console.log(file);
+  });
+});
 
 //Une dos rutas
-const unionPath = path.join(folder,'folder2');
+const unionPath = path.join(documentDirection, "folder2");
 console.log(unionPath);
 
-// recorrer todo y hacer una funcion que tenga todos los links. 
-// recursividad 
-function reverse (str) {
-  if (str === "") {
-      return "";
-  } else {
-      return reverse(str.substr(1)) + str.charAt(0);
-  }
+// recorrer todo y hacer una funcion que tenga todos los links.
+// recursividad
+// Node.js program to demonstrate the
+// fs.stat() method
+
+// Import the filesystem module
+const fs = require('fs');
+
+// Getting information for a file
+fs.stat("example_file.txt", (error, stats) => {
+if (error) {
+	console.log(error);
 }
+else {
+	console.log("Stats object for: example_file.txt");
+	console.log(stats);
 
-let reverseStringIs = reverse('freeCodeCamp');
-console.log(reverseStringIs)
+	// Using methods of the Stats object
+	console.log("Path is file:", stats.isFile());
+	console.log("Path is directory:", stats.isDirectory());
+}
+});
 
+// Getting information for a directory
+fs.stat("example_directory.txt", (error, stats) => {
+if (error) {
+	console.log(error);
+}
+else {
+	console.log("Stats object for: example_directory.txt");
+	console.log(stats);
+
+	// Using methods of the Stats object
+	console.log("Path is file:", stats.isFile());
+	console.log("Path is directory:", stats.isDirectory());
+}
+});
