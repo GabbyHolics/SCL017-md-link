87e94577995c8f4de1e19aea3e21b656c3ceaba4
GabbyHolics <karenthgabriela@gmail.com>
2021-09-09 09:39:27 -0300

    add cli

--- cli.js
+++ cli.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
-const { mdLinks } = require('./md-links.js');
+const { mdLinks } = require('./index.js');
 const path = require("path");
 const routeDirection = path.resolve(process.argv[2]);
 const option = process.argv[3];
 
 mdLinks(routeDirection, {
     validate: option === '--validate' ? true : false
-}).then(res => console.log(res))
+})
+.then(res => console.log(res))
+.catch(err => console.log(err))
 
 
 
--- index.js
+++ index.js
@@ -1,8 +1,156 @@
-const { mdLinks } = require('./md-links.js');
+// 1. obtener ruta
+const fs = require("fs");
 const path = require("path");
-const routeDirection = path.resolve(process.argv[2]);
-const option = process.argv[3];
+const markdownLinkExtractor = require("markdown-link-extractor");
+const axios = require("axios");
 
-mdLinks(routeDirection, {
-    validate: option === '--validate' ? true : false
-}).then(res => console.log(res))
+const linkExtractor = (dataFile, pathInitial) => {
+  const links = markdownLinkExtractor(dataFile, true).filter(
+    (link) => link.href.includes("https://") || link.href.includes("http://")
+  );
+  let arrayLinks = [];
+  links.forEach((link) => {
+    const objectLinks = {
+      file: pathInitial,
+      link: link.href,
+      text: link.text,
+    };
+    arrayLinks.push(objectLinks);
+  });
+  return arrayLinks;
+};
+
+const readFile = (filePath) => {
+  return new Promise((resolve) => {
+    readFilePromise(filePath).then((data) =>
+      resolve(linkExtractor(data, filePath))
+    );
+  });
+};
+
+const dirFile = (doc) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const fileExt = path.extname(doc.toLowerCase());
+      const mdExt = ".md";
+      if (fileExt === mdExt) {
+        readFile(doc).then((links) => resolve(links));
+      } else {
+        console.error("no es md");
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+const readFilePromise = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const readFolderPromise = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(filePath, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+};
+// retornar el result 
+const dirFolder = (dirPath) => {
+  const pathFolder = dirPath;
+  readFolderPromise(pathFolder)
+    .then((files) => {
+      files.forEach((file) => {
+        const fullPath = path.join(pathFolder, file);
+        documentOrFolder(fullPath).then((res) => console.log( 'hola' + res));
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const documentOrFolder = (routeDirection) => {
+  return new Promise((resolve, reject) => {
+    fs.stat(routeDirection, (error, stats) => {
+      if (error) {
+        reject(error);
+      }
+      if (stats.isDirectory()) {
+        resolve(dirFolder(routeDirection));
+      } else if (stats.isFile()) {
+        dirFile(routeDirection).then((links) => resolve(links));
+      }
+    });
+  });
+};
+// let promises = new promises
+
+const getStatusLinks = (routeDirection) => {
+  return new Promise((res) => {
+    return axios
+      .get(routeDirection.link)
+      .then((response) => {
+        routeDirection.status = response.status;
+        if (response.status === 200) {
+          routeDirection.ok = response.statusText;
+        }
+        res(routeDirection);
+      })
+      .catch((error) => {
+        routeDirection.status === 404;
+        routeDirection.ok = "fail";
+        res(routeDirection);
+      });
+  });
+};
+const searchLinks = (routeDirection) => {
+  const promiseArray = [];
+  return new Promise((res, reject) => {
+    documentOrFolder(routeDirection)
+      .then((links) => {
+        links.map((link) => {
+          promiseArray.push(getStatusLinks(link));
+        });
+        Promise.all(promiseArray)
+          .then((result) => res(result))
+          .catch((err) => console.log(err)); // pormises .all
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+const mdLinks = (routeDirection, options = { validate: false }) => {
+  return new Promise((res, reject) => {
+    try {
+      if (!options.validate) {
+        documentOrFolder(routeDirection).then(arrayLinksData => res(arrayLinksData));
+      } else if (options.validate) {
+        searchLinks(routeDirection).then((validateLinks) => res(validateLinks));
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+// documentOrFolder(routeDirection).then(link => console.log(link)) ;
+
+module.exports = {
+  mdLinks,
+  searchLinks,
+  getStatusLinks,
+  documentOrFolder,
+};
